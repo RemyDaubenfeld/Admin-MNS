eadbb2596eb74201ded34b08e5d952f49b6a2f73
RemyDaubenfeld <147379578+RemyDaubenfeld@users.noreply.github.com>
2024-04-30 15:10:21 +0200

    second commit

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+function isActive($currentPage, $pageName)
+{
+    return ($currentPage === $pageName) ? 'is-active' : '';
+}
+
+if(!empty($_SESSION['user_id']) || (isset($_GET['page']) && ($_GET['page']) == 'forgotten_pwd') || (isset($_GET['page']) && $_GET['page'] == 'reset_pwd'))
+{
+    $page = !empty($_GET['page']) ? ($_GET['page']) : 'index';
+}
+else
+{
+    $page = 'connection';
+}
+
+$path = '../src/pages/' . $page . '.php';
+
+if(file_exists($path))
+{   
+    require '../src/data/db-connect.php';
+    require "../src/pages/$page.php";
+    require '../templates/layout.html.php';
+}
+else
+{
+    header('HTTP/1.1 404 Not Found');
+    require '../templates/404.html.php';
+}
+
